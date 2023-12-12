d94dd70884fc04818b10648721a9abd5db1d4eed
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-12 10:51:18 +0100

    add timestamps add export

--- backend/model/User/User.js
+++ backend/model/User/User.js
@@ -2,66 +2,79 @@ const mongoose = require("mongoose");
 
 //create schema
 
-const userSchema = new mongoose.Schema({
-  firstname: {
-    typeof: "string",
-    required: [true, "First Name is required"],
-  },
-  lastname: {
-    typeof: "string",
-    required: [true, "Last Name is required"],
-  },
-  profilePhoto: {
-    typeof: "string",
-  },
-  email: {
-    typeof: "string",
-    required: [true, "Email is required"],
-  },
-  password: {
-    typeof: "string",
-    required: [true, "Password is required"],
-  },
-  postCount: {
-    type: Number,
-    default: 0,
-  },
-  isBlocked: {
-    type: Boolean,
-    default: false,
-  },
-  isAdmin: {
-    typeof: Boolean,
-    default: false,
-  },
-  role: {
-    type: String,
-    enum: ["Admin", "User", "Editor"],
-  },
-  viewedBy: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      typeof: "string",
+      required: [true, "First Name is required"],
     },
-  ],
-  followers: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+    lastname: {
+      typeof: "string",
+      required: [true, "Last Name is required"],
     },
-  ],
-  following: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+    profilePhoto: {
+      typeof: "string",
     },
-  ],
-  active:{
-    type:Boolean,
-    default:true,
+    email: {
+      typeof: "string",
+      required: [true, "Email is required"],
+    },
+    password: {
+      typeof: "string",
+      required: [true, "Password is required"],
+    },
+    postCount: {
+      type: Number,
+      default: 0,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      typeof: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "User", "Editor"],
+    },
+    viewedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
   },
-  posts:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Post"
-  }]
-});
+  {
+    timestamps: true,
+  }
+);
+
+//Compile the user model
+
+const User = mongoose.model("User,userShema");
+
+module.exports = User;
