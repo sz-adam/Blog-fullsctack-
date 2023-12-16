0952605e90a1269174b3072251a6fac21ad6e2bf
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-16 11:37:11 +0100

    database name

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 //function to connect
 const dbConnect = async() =>{
     try{
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL,  { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('DB connected Succes')
     } catch(error){
         console.log(error.message);
