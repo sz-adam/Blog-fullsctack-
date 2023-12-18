22d2dbb6521a3c11d2ed696e4bc230f6e4bec818
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-18 18:27:47 +0100

    delete useNewUrlParser useUnifiedTopology

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 //function to connect
 const dbConnect = async() =>{
     try{
-        await mongoose.connect(process.env.MONGODB_URL,  { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log('DB connected Succes')
     } catch(error){
         console.log(error.message);
