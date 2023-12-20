81ad38e5b2ff1662008b8a61aa51d2e2c155e4db
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-20 18:34:10 +0100

    the user is active or inactive for 30 days

--- backend/model/User/User.js
+++ backend/model/User/User.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
     //image url and then fix it when connecting the frontend
     profilePhoto: {
       type: String,
-      default: '/default-profile-photo.jpg',
+      default: "/default-profile-photo.jpg",
     },
     email: {
       type: String,
@@ -70,77 +70,94 @@ const userSchema = new mongoose.Schema(
         ref: "User",
       },
     ],
-    //plan: 
-     // {
-     //   type: String,
+    //plan:
+    // {
+    //   type: String,
     //    enum: ["Free", "Premium", "Pro"],
     //    default: "Free",
     //  },
-  
-    userAward:  {
+
+    userAward: {
       type: String,
       enum: ["Bronze", "Silver", "Gold"],
-      default:"Bronze"
+      default: "Bronze",
     },
   },
   {
     timestamps: true,
-    toJSON:{virtuals:true},
+    toJSON: { virtuals: true },
   }
 );
 
 //Hooks
-//pre-before record is saved 
-userSchema.pre("findOne",async function(next) {
-  //get the user id 
+//pre-before record is saved
+userSchema.pre("findOne", async function (next) {
+  //get the user id
   const userId = this._conditions._id;
-  //find the post created by the user 
-  const posts =await Post.find({user:userId});
-  //get the last post created by the user 
-  const lastPost = posts[posts.length -1]
+  //find the post created by the user
+  const posts = await Post.find({ user: userId });
+  //get the last post created by the user
+  const lastPost = posts[posts.length - 1];
   //get the last post date
-  const lastPostDate = new Date(lastPost.createdAt); 
-  //get the last post date in string format 
-  const lastPostDateStr =lastPostDate.toDateString();
+  const lastPostDate = new Date(lastPost.createdAt);
+  //get the last post date in string format
+  const lastPostDateStr = lastPostDate.toDateString();
   //add virtuals to the schema
-  userSchema.virtual("lastPostDate").get(function() {
+  userSchema.virtual("lastPostDate").get(function () {
     return lastPostDateStr;
   });
 
-  next()
-})
+  //---------------- Check if user is inactive for 30 days ------------------------
+  //get current date
+  const currentDate = new Date();
+  //get the difference between the last post date and the current day
+  const diff = currentDate - lastPostDate;
+  //get the difference in days and return less than is days
+  const diffInDays = diff / (1000 * 3600 * 24);
+  if (diffInDays > 30) {
+    //Add virtuals is Inactive to the schema to check if user is inactive for 30 days
+    userSchema.virtual("isInactive").get(function () {
+      return true;
+    });
+  } else{
+    userSchema.virtual("isInactive").get(function () {
+      return false;
+    });
+
+  }
+
+  next();
+});
 
 //post -after saving
-userSchema.post("save", function(next) {
-  
-})
+userSchema.post("save", function (next) {});
 
-//Get fullname 
-userSchema.virtual("fullname").get(function(){
-  return `${this.firstname} ${this.lastname}`
+//Get fullname
+userSchema.virtual("fullname").get(function () {
+  return `${this.firstname} ${this.lastname}`;
 });
 
 //Get posts count
-userSchema.virtual("postCounts").get(function(){
+userSchema.virtual("postCounts").get(function () {
   return this.posts.length;
 });
 
 //Get followers
-userSchema.virtual("followersCount").get(function(){
+userSchema.virtual("followersCount").get(function () {
   return this.followers.length;
 });
 
 //Get following
-userSchema.virtual("followingCount").get(function(){
+userSchema.virtual("followingCount").get(function () {
   return this.following.length;
 });
 //Get viewers count
-userSchema.virtual("viewersCount").get(function(){
+userSchema.virtual("viewersCount").get(function () {
   return this.viewers.length;
 });
 
 //Get blocked count
-userSchema.virtual("blockedCount").get(function(){
+userSchema.virtual("blockedCount").get(function () {
   return this.blocked.length;
 });
 
