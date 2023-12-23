22b842bec9fef31787e6f7ea1a63397c5f79db2b
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-23 20:20:08 +0100

    add navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navbar />}/>
-      </Routes>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path="/" />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </Router>
     </>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 function Navbar() {
   return (
     <div className="navbar justify-between p-4">
-      <div >
+      <div>
         <Link to="/">
-          <p className="text-lg font-extrabold bg-gradient-to-r text-transparent from-green-500 to-blue-500 bg-clip-text">MERN Blogs</p>
+          <p className="text-lg font-extrabold bg-gradient-to-r text-transparent from-green-500 to-blue-500 bg-clip-text">
+            MERN Blogs
+          </p>
         </Link>
       </div>
       <div className="flex space-x-4">
-        <div >Login</div>
-        <div >Register</div>
+        <div>
+          <Link to="/login">Login</Link>
+        </div>
+        <div>
+          <Link to="/register">Registration</Link>
+        </div>
       </div>
     </div>
   );
