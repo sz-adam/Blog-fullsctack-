9e9d37520d75d9d1ca5c31ce573bf81766037dec
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-24 11:28:47 +0100

    register basic logic

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,29 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import InputBox from "../components/InputBox";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { IoKeyOutline } from "react-icons/io5";
 import { IoManOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 function Register() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleRegistration = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_API_REGISTER,
+        { firstname, lastname, email, password },
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (response.status === 200) {
+        navigate("/login");
+      
+      }
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center w-full h-[85vh] ">
-      <form className="w-[85%] max-w-[400px]">
+    <div className="flex justify-center items-center w-full h-[85vh]">
+      <form className="w-[85%] max-w-[400px]" onSubmit={handleRegistration}>
         <h1 className="entryText log-reg-color mb-24">Registration</h1>
-        <InputBox type="text" placeholder="First name" icon={IoManOutline} />
-        <InputBox type="text" placeholder="Last name" icon={IoManOutline} />
+        <InputBox
+          type="text"
+          placeholder="First name"
+          icon={IoManOutline}
+          value={firstname}
+          required={true}
+          onChange={(event) => setFirstname(event.target.value)}
+        />
+        <InputBox
+          type="text"
+          placeholder="Last name"
+          icon={IoManOutline}
+          value={lastname}
+          onChange={(event) => setLastname(event.target.value)}
+          required={true}
+        />
         <InputBox
           type="email"
           placeholder="Email"
           icon={MdOutlineMailOutline}
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
-        <InputBox type="password" placeholder="Password" icon={IoKeyOutline} />
+        <InputBox
+          type="password"
+          placeholder="Password"
+          icon={IoKeyOutline}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required={true}
+        />
+        <div className="text-center text-red-600">{error}</div>
         <div className="flex justify-center items-center mt-10">
           <button
             className="border-2 log-reg-color text-white p-3 px-10 rounded-full font-bold"
             type="submit"
           >
-            Login
+            Register
           </button>
         </div>
         <p className="mt-6 text-slate-400 text-lg text-center">
