7cb815961c9512b37636cc8ea77433043f90fc55
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-25 09:07:41 +0100

    display posts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { UserContext } from "../context/userContext";
 
 function Home() {
+  const { user, setUser } = useContext(UserContext);
+  const [posts, setPosts] = useState([]);
+  const [noLoginPosts, setNoLoginPosts] = useState([]);
+  const access_token = user?.data?.token;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (access_token) {
+          const response = await axios.get(import.meta.env.VITE_API_ALLPOSTS, {
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+            },
+          });
+          setPosts(response.data.data);
+        } else {
+          const response = await axios.get(
+            import.meta.env.VITE_API_NOLOGINPOSTS
+          );
+          setNoLoginPosts(response.data.data);
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchData();
+  }, [access_token]);
+  import.meta.env.VITE_API_LOGIN;
   return (
-    <div>Home</div>
-  )
+    <div>
+      {access_token ? (
+        posts.map((post) => (
+          <div key={post._id}>
+            <p>{post.title}</p>
+          </div>
+        ))
+      ) : (
+        <div className="flex space-x-4 items-center">
+          {noLoginPosts.map((post) => (
+            <div key={post._id}>
+              <p>{post.title}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
