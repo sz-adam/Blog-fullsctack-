20eed09a67569c10c8ccb964b1f17b9191cee334
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-26 16:08:17 +0100

    create category

--- frontend/src/pages/WritePost.jsx
+++ frontend/src/pages/WritePost.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import InputBox from "../components/InputBox";
 import PostService from "../services/PostsServices";
 import { UserContext } from "../context/userContext";
+import CategoryService from "../services/CategoryServices";
 
 function WritePost() {
   const [title, setTitle] = useState("");
@@ -11,23 +12,53 @@ function WritePost() {
   const { user, setUser } = useContext(UserContext);
   const access_token = user?.data?.token;
 
-  const handlePostCreate = async (event) => {
+  const handleCategoryCreate = async (event) => {
     event.preventDefault();
-
+  
     try {
-      await PostService.createPost(access_token, {
-        title,
-        description,
-        category,
-        photo,
-      });
+      const categoryData = await CategoryService.createCategory(access_token, {
+        title: title
+      });    
+      setCategory(categoryData)      
+      
     } catch (error) {
       console.log(error);
     }
   };
 
+
+
+
+
+
+ const handlePostCreate = async (event) => {
+  event.preventDefault();
+  try {
+    await PostService.createPost(access_token, {
+      title,
+      description,
+     // category,
+      photo,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
   return (
     <div className="flex justify-center items-center text-center">
+       <form onSubmit={handleCategoryCreate}>
+      <label>
+        Category Title:
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </label>
+      <button type="submit">Create Category</button>
+    </form>
+{/**
       <form className="w-2/3" onSubmit={handlePostCreate}>
         <h1 className="text-3xl font-extrabold bg-gradient-to-r text-transparent from-green-500 to-blue-500 bg-clip-text my-10">
           Write Post
@@ -51,6 +82,7 @@ function WritePost() {
           placeholder="Category"
           value={category}
           onChange={(event) => setCategory(event.target.value)}
+       
         />
         <InputBox
           type="text"
@@ -64,6 +96,7 @@ function WritePost() {
           Sending
         </button>
       </form>
+       */}
     </div>
   );
 }
---
+++ frontend/src/services/CategoryServices.jsx
@@ -0,0 +1,27 @@
+// CategoryService.js
+
+import axios from "axios";
+
+const CategoryService = {
+  createCategory: async (access_token, categoryData) => {
+    try {
+      if (access_token) {
+        const response = await axios.post(
+          import.meta.env.VITE_API_CREATE_CATEGORIES, // Adjust the endpoint accordingly
+          categoryData,
+          {
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+            },
+          }
+        );
+        return response.data.data;
+      }
+    } catch (error) {
+      console.error("Error creating category:", error);
+      throw error;
+    }
+  },
+};
+
+export default CategoryService;
