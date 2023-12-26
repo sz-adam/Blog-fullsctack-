2c2ae66c15cae77eedaa09ce188e1e0b257d7e7d
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-26 14:10:53 +0100

    create input

--- frontend/src/pages/WritePost.jsx
+++ frontend/src/pages/WritePost.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import InputBox from "../components/InputBox";
 
 function WritePost() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [photo, setPhoto] = useState("");
   return (
-    <div>WritePost</div>
-  )
+    <div className="flex justify-center items-center text-center" >
+      <form className="w-2/3 ">
+        <h1 className="text-3xl font-extrabold bg-gradient-to-r text-transparent from-green-500 to-blue-500 bg-clip-text my-10">Write Post</h1>
+        <InputBox
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          required={true}
+        />
+        <InputBox
+          type="text"
+          placeholder="description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          required={true}
+        />
+        <InputBox
+          type="text"
+          placeholder="Category"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+          required={true}
+        />
+        <InputBox
+          type="text"
+          placeholder="Photo"
+          value={photo}
+          onChange={(event) => setPhoto(event.target.value)}
+          required={true}
+        />
+
+        <button type="submit" className="btn-dark">Sending</button>
+      </form>
+    </div>
+  );
 }
 
-export default WritePost
+export default WritePost;
