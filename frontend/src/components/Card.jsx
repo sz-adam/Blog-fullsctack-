98e480c798a3bbce7e0d0cf83e976bc9daebced9
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-28 12:51:18 +0100

    style and moving the update, delete modal

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,15 +1,26 @@
-import React from "react";
-import { AiOutlineLike } from "react-icons/ai";
-import { AiOutlineDislike } from "react-icons/ai";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { FaRegEdit } from "react-icons/fa";
+import DeleteModal from "./DeleteModal";
 
-function Card({ postData }) {
+function Card({ postData, postId }) {
+  const [deleteModal, setDeleteModal] = useState(false);
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateString).toLocaleDateString("en-US", options);
   };
+
+  console.log(postData)
   return (
     <div>
-      <div className="flex justify-end mb-4 mt-2 md:mb-6 items-center ">
+      <div className="flex justify-around mb-4 mt-2 md:mb-6 items-center">
+        <p className="text-gray-500">
+          <Link to="/" className="flex items-center">
+            <FaArrowLeft className="mr-1" /> Back
+          </Link>
+        </p>
         <p className="text-gray-500">
           <span className="font-bold">Create:</span>{" "}
           {formatDate(postData?.createdAt)}
@@ -17,9 +28,9 @@ function Card({ postData }) {
       </div>
 
       {/**post */}
-      <div className="lg:flex justify-center items-center text-center w-full ">
+      <div className="lg:flex justify-center items-center text-center w-full xl:w-3/4 mx-auto pr-2">
         {/** foto*/}
-        <div className="w-full md:w-1/2 lg:w-1/3 flex justify-center mb-4 mx-auto">
+        <div className="w-full md:w-1/2 lg:w-1/3  flex justify-center mb-4 mx-auto">
           <img
             src={postData?.photo}
             alt={postData?.title}
@@ -28,21 +39,26 @@ function Card({ postData }) {
         </div>
         {/**description */}
         <div className="md:w-full lg:w-2/3 mx-auto md:ml-10">
+          <div className="flex justify-end  ">
+            <Link to={`/update/${postId}`}>
+              <FaRegEdit className="icon text-gray-600" />
+            </Link>
+            <AiTwotoneDelete
+              className="icon text-red-600 cursor-pointer"
+              onClick={() => setDeleteModal(true)}
+            />
+          </div>
+          {deleteModal && (
+            <DeleteModal setDeleteModal={setDeleteModal} postId={postId} />
+          )}
+
           <h1 className="text-3xl font-bold mb-2">{postData?.title}</h1>
+          <h2 className="font-medium text-gray-400 mb-1 py-10">
+            CATEGORY: {postData?.title}
+          </h2>
           <p className="text-gray-600 mb-4 text-justify">
             {postData?.description}
           </p>
-
-          <div className="flex items-center space-x-4 mb-4">
-            <span className="text-gray-400 inline-flex items-center leading-none border-r-2 border-gray-200 pr-3">
-              <AiOutlineLike className="mr-1" />
-              <p>{postData?.likesCount}</p>
-            </span>
-            <span className="text-gray-400 inline-flex items-center leading-none">
-              <AiOutlineDislike className="mr-1" />
-              <p>{postData?.disLikesCount}</p>
-            </span>
-          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -3,10 +3,12 @@ import { useParams } from "react-router-dom";
 import PostService from "../services/PostsServices";
 import { UserContext } from "../context/userContext";
 import { useNavigate, Link } from "react-router-dom";
-import DeleteModal from "../components/DeleteModal";
 import CreateComment from "../components/CreateComment";
 import PostAllComment from "../components/PostAllComment";
 import Card from "../components/Card";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
 
 function PostDetails() {
   const { postId } = useParams();
@@ -15,12 +17,7 @@ function PostDetails() {
   const access_token = user?.data?.token;
   const navigate = useNavigate();
   const [deleteModal, setDeleteModal] = useState(false);
-  console.log(postData);
-
-  const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(dateString).toLocaleDateString("en-US", options);
-  };
+  const [showCreateComment, setShowCreateComment] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,8 +41,8 @@ function PostDetails() {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col w-11/12 justify-center items-center ">
-        <Card postData={postData} />
-
+        <Card postData={postData} postId={postId} />
+        {/**
         <div className="mt-10">
           <Link to="/" className="btn-dark">
             Back
@@ -55,25 +52,35 @@ function PostDetails() {
           </Link>
           <Link to="/" className="btn-dark">
             Dislike
-          </Link>
-          <button className="btn-dark">Comments</button>
-          <Link to={`/update/${postId}`} className="btn-dark">
-            Update
-          </Link>
-          <button
-            className="btn-dark"
-            type="button"
-            onClick={() => setDeleteModal(true)}
-          >
+          </Link>        
+         
             Delete
           </button>
-          {deleteModal && (
-            <DeleteModal setDeleteModal={setDeleteModal} postId={postId} />
-          )}
+        
         </div>
-        <CreateComment postId={postId} />
+          <CreateComment postId={postId} />
+ */}
+
+        <div className="flex items-center justify-center text-center space-x-4 mb-4 ">
+          <span className="text-gray-400 inline-flex items-center leading-none border-r-2 border-gray-200 pr-3 text-xl">
+            <AiOutlineLike className="mr-1" />
+            <p>{postData?.likesCount}</p>
+          </span>
+          <span className="text-gray-400 inline-flex items-center leading-none  text-xl border-r-2 border-gray-200 pr-3">
+            <AiOutlineDislike className="mr-1" />
+            <p>{postData?.disLikesCount}</p>
+          </span>
+          <span
+            className="text-gray-400 inline-flex items-center leading-none  text-xl cursor-pointer"
+            onClick={() => setShowCreateComment(true)}
+          >
+            <FaRegComment className="mr-1" />
+            <p>Comment</p>
+          </span>
+          {showCreateComment ? <CreateComment postId={postId} /> : null}
+        </div>
+
         <PostAllComment postId={postId} />
-       
       </div>
     </div>
   );
