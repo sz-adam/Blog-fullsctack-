028df89114b39b94e6fc61844c3bbccf8ad92770
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-28 17:32:49 +0100

    Creating an edit section

--- frontend/src/components/PostAllComment.jsx
+++ frontend/src/components/PostAllComment.jsx
@@ -1,27 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { AiTwotoneDelete } from "react-icons/ai";
 import { FaRegEdit } from "react-icons/fa";
 
-function PostAllComment({ comments }) {
+import InputBox from "./InputBox";
+
+function PostAllComment({ comments, updateComments }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedComment, setEditedComment] = useState("");
+  const [editedCommentId, setEditedCommentId] = useState(null);
+
+
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateString).toLocaleDateString("en-US", options);
   };
 
+  const handleCommentUpdate = async () => {
 
+  };
 
-  console.log(comments);
+  const openEditing = (commentId, commentDescription) => {
+    setIsEditing(true);
+    setEditedCommentId(commentId);
+    setEditedComment(commentDescription);
+  };
+
+  const closeEditing = () => {
+    setIsEditing(false);
+    setEditedCommentId(null);
+    setEditedComment("");
+  };
+
   return (
     <div className="w-full md:w-1/2 p-4">
       {comments.map((comment) => (
-        <div key={comment._id} className="mb-2 p-2 bg-gray-100 flex rounded-lg">
+        <div key={comment._id} className="mb-2 p-2 bg-gray-100 flex ">
           <div className="flex-grow">
-            <p>{comment.description}</p>
+            {isEditing && editedCommentId === comment._id ? (
+              <InputBox
+                type="text"
+                value={editedComment}
+                onChange={(e) => setEditedComment(e.target.value)}
+              />
+            ) : (
+              <p>{comment.description}</p>
+            )}
           </div>
           <div className="flex flex-col justify-end ml-2">
             <div className="flex mb-1">
-              <FaRegEdit className="icon text-gray-600" />
+              {isEditing && editedCommentId === comment._id ? (
+                <div>
+                   <button onClick={handleCommentUpdate}>Save</button>
+                <button onClick={closeEditing}>Cancel</button>
+                </div>
+               
+              ) : (
+                <FaRegEdit
+                  className="icon text-gray-600"
+                  onClick={() => openEditing(comment._id, comment.description)}
+                />
+              )}
               <AiTwotoneDelete className="icon text-red-600 cursor-pointer" />
             </div>
             <div>
