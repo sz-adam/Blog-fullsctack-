d82beb2657b73f13870cd66e053973530f99beac
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-12-29 22:11:48 +0100

    add all user

--- frontend/src/services/UserServices.jsx
+++ frontend/src/services/UserServices.jsx
@@ -44,6 +44,25 @@ const UserService = {
       throw error;
     }
   },
+
+  allUser: async (access_token) => {
+    try {
+      if (access_token) {
+        const response = await axios.get(
+          import.meta.env.VITE_API_ALL_USER,
+          {
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+            },
+          }
+        );
+        return response.data.data;
+      }
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      throw error;
+    }
+  },
 };
 
 export default UserService;
