def210f88d003a6cc903489c639d52b3644add7e
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-01-02 12:27:03 +0100

    userProfile was included in the context

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,6 +1,29 @@
-import { createContext } from "react";
-export const UserContext = createContext({
-  user: null,
+import { createContext, useEffect, useState  } from "react";
+import UserService from "../services/UserServices";
+import { getAccessToken } from "../common/utils";
+export const UserContext = createContext({});
 
-  setUser: () => {},
-});
+export function UserContextProvider({children}) {
+  const [user, setUser] =useState(null)
+  const access_token = getAccessToken();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (access_token) {
+          const userData = await UserService.userProfile(access_token);
+          setUser(userData);
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+    fetchData();
+  }, [access_token]);
+
+
+
+return (<UserContext.Provider value={{user,setUser}}>
+  {children}
+</UserContext.Provider>)
+}
