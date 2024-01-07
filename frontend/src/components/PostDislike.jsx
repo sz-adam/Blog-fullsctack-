fa7452819770dc3d3ae8e1d0d31b3e1d09470475
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-01-07 11:48:56 +0100

    dislike function

--- frontend/src/components/PostDislike.jsx
+++ frontend/src/components/PostDislike.jsx
@@ -1,17 +1,41 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { AiFillDislike } from "react-icons/ai";
-
+import { getAccessToken } from "../common/utils";
+import { UserContext } from "../context/UserContext";
+import { useParams } from "react-router-dom";
+import PostService from "../services/PostsServices";
 
 function PostDislike({ post, setPost }) {
-  const [isDisliked, setIsDisliked] = useState(false);
+  const access_token = getAccessToken();
+  const { user, setUser } = useContext(UserContext);
+  const { postId } = useParams();
+
+  //ellenörzi az aktuális user Id-ét a disLikes tömbben ha nincs benne beállítja a kéksszint
+  //ha benne van nem csinál semmit se
+  const isUserDislikedPost = () => {
+    return post?.disLikes?.includes(user?.id);
+  };
+
+  const handleDisLikePost = async () => {
+    try {
+      if (access_token && postId) {
+        const disLikePost = await PostService.disLikePost(access_token, postId);
+        setPost(disLikePost);
+      }
+    } catch (error) {
+      console.error("Error fetching post details or posts:", error);
+    }
+  };
 
   return (
     <div
       className="pl-10 cursor-pointer flex justify-center items-center"
-      onClick={() => setIsDisliked(!isDisliked)}
+      onClick={handleDisLikePost}
     >
-      <AiFillDislike 
-        className={`text-3xl mr-1 ${isDisliked ? "text-blue-500" : ""}`}
+      <AiFillDislike
+        className={`text-3xl mr-1 ${
+          isUserDislikedPost() ? "text-blue-500" : ""
+        }`}
       />
       <sub>{post?.disLikesCount}</sub>
     </div>
