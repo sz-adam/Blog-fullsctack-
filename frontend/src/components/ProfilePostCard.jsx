85fa0084e1e39602dd2854e1bdd840f64c7ad804
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-01-07 14:56:58 +0100

    like % display

--- frontend/src/components/ProfilePostCard.jsx
+++ frontend/src/components/ProfilePostCard.jsx
@@ -1,19 +1,27 @@
 import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa6";
 
 function ProfilePostCard({ userCard }) {
- 
   return (
     <div className="p-5">
       <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-      
-          <img className="rounded-t-lg h-60 w-60" src={userCard?.photo} alt="" />
-       
+        <img className="rounded-t-lg h-60 w-60" src={userCard?.photo} alt="" />
+
         <div className="p-5">
-         
+          <div className="flex justify-between">
             <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
               {userCard?.title}
             </h5>
-      
+            {userCard?.likesPercentage === "NaN%" ||
+            userCard?.likesPercentage === "0%" ? (
+              ""
+            ) : (
+              <div className="flex justify-end items-center">
+                <FaHeart className="text-red-600 mr-2 animate-pulse" />
+                {userCard?.likesPercentage}
+              </div>
+            )}
+          </div>
 
           <Link
             to={`/post/${userCard?.id}`}
