e1c677ed0b890d6d58d94d831657f346991c86e9
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-01-11 23:51:11 +0100

    getting followers users

--- frontend/src/components/UserFollowers.jsx
+++ frontend/src/components/UserFollowers.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getAccessToken } from '../common/utils';
+import UserService from '../services/UserServices';
 
 function UserFollowers() {
+
+  const access_token = getAccessToken();
+
+  useEffect(() => {
+    const followUserData = async () => {
+      try {
+        if (access_token) {
+          const followuser = await UserService.followersArray(access_token);
+          console.log(followuser);
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+    followUserData();
+  }, [access_token]);
+
   return (
     <div>UserFollowers</div>
   )
