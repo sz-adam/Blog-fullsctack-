1b38abc904917fb2d4871dd5773b35506e4cd6da
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-01-26 11:12:29 +0100

    new inactive virtual shema

--- backend/model/User/User.js
+++ backend/model/User/User.js
@@ -105,76 +105,6 @@ userSchema.pre("findOne", async function (next) {
   const userId = this._conditions._id;
   //find the post created by the user
   const posts = await Post.find({ user: userId });
-  //get the last post created by the user
-  const lastPost = posts[posts.length - 1];
-  //get the last post date
-  const lastPostDate = new Date(lastPost && lastPost.createdAt);
-  //get the last post date in string format
-  const lastPostDateStr = lastPostDate.toDateString();
-  //add virtuals to the schema
-  userSchema.virtual("lastPostDate").get(function () {
-    return lastPostDateStr;
-  });
-
-  //---------------- Check if user is inactive for 30 days ------------------------
-  //get current date
-  const currentDate = new Date();
-  //get the difference between the last post date and the current day
-  const diff = currentDate - lastPostDate;
-  //get the difference in days and return less than is days
-  const diffInDays = diff / (1000 * 3600 * 24);
-  if (diffInDays > 30) {
-    //Add virtuals is Inactive to the schema to check if user is inactive for 30 days
-    userSchema.virtual("isInactive").get(function () {
-      return true;
-    });
-    //Find the user by ID and update
-    await User.findByIdAndUpdate(
-      userId,
-      {
-        isBlocked: true,
-      },
-      {
-        new: true,
-      }
-    );
-  } else {
-    userSchema.virtual("isInactive").get(function () {
-      return false;
-    });
-    //Find the user by ID and update
-    await User.findByIdAndUpdate(
-      userId,
-      {
-        isBlocked: false,
-      },
-      {
-        new: true,
-      }
-    );
-  }
-
-  //---------- Last activa Date ------------
-
-  //convert to days ago, for example 1 day ago
-  const daysAgo = Math.floor(diffInDays);
-  //add virtuals lastActive in days to the schema
-  userSchema.virtual("lastActive").get(function () {
-    //check if daysAgo is less than 0
-    if (daysAgo <= 0) {
-      return "Today";
-    }
-    //check if daysAgo is equal to 1
-    if (daysAgo === 1) {
-      return "Yesterday";
-    }
-    //check if daysAgo is greater than 1
-    if (daysAgo > 1) {
-      return `${daysAgo} days ago `;
-    }
-  });
-
-  //----------------------------------------------
   //Update userAward based on the number of posts
   //--------------------------------------------
   //get the number of posts
@@ -252,7 +182,7 @@ userSchema.virtual("blockedCount").get(function () {
   return this.blocked.length;
 });
 userSchema.virtual("lastLogin").get(function () {
-  const lastLogin= this?.updatedAt
+  const lastLogin = this?.updatedAt;
   const currentDate = new Date();
   const diff = currentDate - lastLogin;
   const diffInDays = diff / (1000 * 3600 * 24);
@@ -267,6 +197,17 @@ userSchema.virtual("lastLogin").get(function () {
   //check if daysAgo is greater than 1
   if (daysAgo > 1) {
     return `${daysAgo} days ago `;
+  }
+});
+userSchema.virtual("isInactive").get(function () {
+  const lastLogin = this?.updatedAt;
+  const currentDate = new Date();
+  const diff = currentDate - lastLogin;
+  const diffInDays = diff / (1000 * 3600 * 24);
+  if(diffInDays > 5) {
+   return "The user is inactive, has not logged in for 5 days."
+  } else {
+    return null
   }
 });
 
