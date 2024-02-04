102f9976bf693d49dd1de6c2713392edd4c36c03
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-04 10:12:25 +0100

    new style userAward display

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -10,75 +10,79 @@ function UserProfile() {
   const { user, setUser } = useContext(UserContext);
   const awardColor = () => {
     if (user?.userAward === "Bronze") {
-      return "text-8xl text-orange-500 ";
+      return "w-40 h-40 text-orange-500 ";
     } else if (user?.userAward === "Silver") {
-      return "text-8xl text-stone-300";
+      return "w-40 h-40 text-stone-300";
     } else if (user?.userAward === "Gold") {
-      return "text-8xl text-yellow-400";
-    } 
+      return "w-40 h-40 text-yellow-400";
+    }
   };
   return (
     <>
-     <AnimatedMotion animationName="pageAnimation">
-      <div className="mt-5 ml-5 icon text-lg">
-        <Link to="/">
-          <FaArrowLeft className="mr-1 " />
-        </Link>
-      </div>
-      <div className="p-4 md:p-10 flex justify-center items-center">
-        <div className="pt-8 bg-white mt-5">
-          <div className="grid grid-cols-1 md:grid-cols-3">
-            <div className="md:grid md:grid-cols-4 flex justify-around text-center order-last md:order-first mt-20 md:mt-0">
-              <div>
-                <p className="font-bold text-gray-700 text-xl">
-                  {user?.followersCount}
-                </p>
-                <p className="text-gray-400">Followers</p>
-              </div>
-              <div>
-                <p className="font-bold text-gray-700 text-xl">
-                  {user?.followingCount}
-                </p>
-                <p className="text-gray-400">Following</p>
-              </div>
-              <div>
-                <p className="font-bold text-gray-700 text-xl">
-                  {user?.posts?.length}
-                </p>
-                <p className="text-gray-400">Post</p>
-              </div>
-
-              <div>
-                <p className="font-bold text-gray-700 text-xl">
-                  {user?.userAward}
-                </p>
-                <p className="text-gray-400">User Award</p>
-              </div>
-            </div>
-            <div className="relative">
-              <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
-                <img
-                  src={user?.profilePhoto}
-                  alt=""
-                  className="rounded-full w-48 h-48"
-                />
-              </div>
-            </div>
-
-            <div className="mx-auto">
-             <p>  <TfiCup className={awardColor()} /></p>
-             <p className="text-center pt-3 font-bold text-gray-700 text-xl"> {user?.userAward}</p>
-            </div>
-          </div>
-          <div className="mt-20 text-center border-b pb-12">
-            <h1 className="text-4xl font-medium text-gray-700">
-              {user?.fullname}
-            </h1>
-            <p>{user?.email}</p>
-          </div>
-          <UserProfileNavigation />
+      <AnimatedMotion animationName="pageAnimation">
+        <div className="mt-5 ml-5 icon text-lg">
+          <Link to="/">
+            <FaArrowLeft className="mr-1 " />
+          </Link>
         </div>
-      </div>
+        <div className="flex-col flex md:flex-row justify-evenly items-center pt-10">
+          <img
+            src={user?.profilePhoto}
+            alt=""
+            className="rounded-full w-48 h-48 "
+          />
+
+          <div className="mt-10">
+            <p>
+              <TfiCup className={awardColor()} />
+            </p>
+            <p className="text-center pt-3 font-bold text-gray-700 text-xl">
+              {user?.userAward}
+            </p>
+          </div>
+        </div>
+        <div className="mt-20 text-center border-b pb-12">
+              <h1 className="text-4xl font-medium text-gray-700">
+                {user?.fullname}
+              </h1>
+              <p>{user?.email}</p>
+            </div>
+        
+        <div className="p-4 md:p-10 flex justify-center items-center">
+          <div className="pt-8 bg-white md:mt-5 mt-0">
+            <div className=" my-auto mx-auto">
+              <div className="md:grid md:grid-cols-4 flex justify-around text-center order-last md:order-first mt-20 md:mt-0">
+                <div>
+                  <p className="font-bold text-gray-700 text-xl">
+                    {user?.followersCount}
+                  </p>
+                  <p className="text-gray-400">Followers</p>
+                </div>
+                <div>
+                  <p className="font-bold text-gray-700 text-xl">
+                    {user?.followingCount}
+                  </p>
+                  <p className="text-gray-400">Following</p>
+                </div>
+                <div>
+                  <p className="font-bold text-gray-700 text-xl">
+                    {user?.posts?.length}
+                  </p>
+                  <p className="text-gray-400">Post</p>
+                </div>
+
+                <div>
+                  <p className="font-bold text-gray-700 text-xl">
+                    {user?.userAward}
+                  </p>
+                  <p className="text-gray-400">User Award</p>
+                </div>
+              </div>
+            </div>
+           
+            <UserProfileNavigation />
+          </div>
+        </div>
       </AnimatedMotion>
     </>
   );
