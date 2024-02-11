7021b9fcc134828f983c8eb497a20f1460c55bca
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-11 12:11:12 +0100

    add sorted

--- frontend/src/components/SortedPost.jsx
+++ frontend/src/components/SortedPost.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function SortedPost() {
+function SortedPost({ sortBy, handleSortChange }) {
   return (
-    <div>SortedPost</div>
-  )
+    <div className="flex items-center justify-center md:justify-start md:px-10 mb-4 space-x-2">
+      <label className="text-gray-700">Sort by:</label>
+      <select
+        className="appearance-none border border-blue-400 px-4 py-2 rounded-full"
+        value={sortBy}
+        onChange={(e) => handleSortChange(e.target.value)}
+      >
+        <option value="date">Date </option>
+        <option value="likes">Likes</option>
+        <option value="comments">Comments</option>
+        <option value="views">Views</option>
+        <option value="postName">Post name</option>
+        <option value="userName">User name</option>
+      </select>
+    </div>
+  );
 }
 
-export default SortedPost
+export default SortedPost;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,11 +4,16 @@ import CarouselHome from "../components/CarouselHome";
 import { getAccessToken } from "../common/utils";
 import PostService from "../services/PostsServices";
 import AnimatedMotion from "../common/AnimatedMotion";
+import SortedPost from "../components/SortedPost";
 
 function Home({ searchPost }) {
   const access_token = getAccessToken();
   const [posts, setPosts] = useState([]);
-  const [sortedPosts, setSortedPosts] = useState([]);
+  const [sortBy, setSortBy] = useState("date");
+  console.log(posts[0]);
+  const handleSortChange = (selectedValue) => {
+    setSortBy(selectedValue);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,16 +24,53 @@ function Home({ searchPost }) {
         } else {
           fetchedPosts = await PostService.getAllPosts();
         }
-        //sorted date
-        const sortedByDate = fetchedPosts.slice().sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-        setPosts(sortedByDate);
+
+        let sortedPosts;
+        // sort date
+        if (sortBy === "date") {
+          sortedPosts = fetchedPosts.sort(
+            (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+          );
+        }
+        // sort likes
+        else if (sortBy === "likes") {
+          sortedPosts = fetchedPosts.sort(
+            (a, b) => b.likesCount - a.likesCount
+          );
+        }
+        // sort comments
+        else if (sortBy === "comments") {
+          sortedPosts = fetchedPosts.sort(
+            (a, b) => b.comments.length - a.comments.length
+          );
+        }
+        // sort views
+        else if (sortBy === "views") {
+          sortedPosts = fetchedPosts.sort(
+            (a, b) => b.viewsCount - a.viewsCount
+          );
+        }
+        // sort postName
+        else if (sortBy === "postName") {
+          sortedPosts = fetchedPosts.sort((a, b) =>
+            a.title.localeCompare(b.title)
+          );
+        }
+        //sort username
+        else if (sortBy === "userName") {
+          sortedPosts = fetchedPosts.sort((a, b) =>
+            a.user.fullname.localeCompare(b.user.fullname)
+          );
+        }
+
+        setPosts(sortedPosts);
       } catch (error) {
         console.error("Error fetching posts:", error);
       }
     };
-  
+
     fetchData();
-  }, [access_token, searchPost]);
+  }, [access_token, searchPost, sortBy]);
 
   const filteredPosts = posts.filter((post) =>
     post.title.toLowerCase().includes(searchPost.toLowerCase())
@@ -36,22 +78,23 @@ function Home({ searchPost }) {
 
   return (
     <>
-     <AnimatedMotion animationName="pageAnimation">
-     {filteredPosts.length > 0 && <CarouselHome posts={filteredPosts} />}
-      {filteredPosts.length === 0 ? (
-        <div className="flex items-center justify-center h-screen">
-          <h1 className="text-2xl md:text-6xl text-center font-extrabold text-transparent bg-clip-text log-reg-color">
-            There are no results for your search criteria
-          </h1>
-        </div>
-      ) : (
-        <div className="grid  sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 ">
-          {filteredPosts.map((post) => (
-            <PostCard key={post.id} post={post} />
-          ))}
-        </div>
-      )}
-         </AnimatedMotion>
+      <AnimatedMotion animationName="pageAnimation">
+        {filteredPosts.length > 0 && <CarouselHome posts={filteredPosts} />}
+        <SortedPost sortBy={sortBy} handleSortChange={handleSortChange} />
+        {filteredPosts.length === 0 ? (
+          <div className="flex items-center justify-center h-screen">
+            <h1 className="text-2xl md:text-6xl text-center font-extrabold text-transparent bg-clip-text log-reg-color">
+              There are no results for your search criteria
+            </h1>
+          </div>
+        ) : (
+          <div className="grid  sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 ">
+            {filteredPosts.map((post) => (
+              <PostCard key={post.id} post={post} />
+            ))}
+          </div>
+        )}
+      </AnimatedMotion>
     </>
   );
 }
