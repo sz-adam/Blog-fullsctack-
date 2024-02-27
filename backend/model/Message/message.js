b9134e0f0a15e8eca9751b66f0142678180d1c8d
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-27 18:07:08 +0100

    message model

---
+++ backend/model/Message/message.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const messageShema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message =mongoose.model("Message", messageShema);
+
+module.exports = Message;
