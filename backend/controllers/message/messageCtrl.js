73f47a0b6bd5614d4a5bd3b7de1ea07c0a1bf8b8
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-28 18:59:28 +0100

    add messages function

--- backend/controllers/message/messageCtrl.js
+++ backend/controllers/message/messageCtrl.js
@@ -1,32 +1,32 @@
 const Message = require("../../model/Message/message");
-const User = require("../../model/User/User")
+const User = require("../../model/User/User");
 const { appErr } = require("../../utils/appErr");
 
 const createAdminMessage = async (req, res, next) => {
   const { message } = req.body;
   try {
-    const user = await User.findById(req.userAuth);
     const admin = await User.findOne({ isAdmin: true });
-    console.log(admin.firstname)
-
-    //mi történjen ha a felhasználo blokkolva van? 
-    if (user.isBlocked) {
-    
+    //mi történjen ha a felhasználo blokkolva van?
+    const blockedUser = await User.findById(req.userAuth);
+    if (blockedUser.isBlocked) {
+      const newMessages = await Message.create({ message, user: req.userAuth });
+      admin.message.push(newMessages);
+      await admin.save();
+      res.json({
+        status: "success",
+        data: newMessages,
+      });
+    } else {
+      res.status(403).json({
+        status: "error",
+        message:
+          "The user is not blocked and cannot send message",
+      });
     }
-
-   
-  
-
-    res.status(200).json({
-      status: "success",
-       //data:message
-      message: "Az üzenet sikeresen létrehozva.",
-    });
   } catch (error) {
     console.error(error);
     return next(appErr(error.message));
   }
 };
-
 
 module.exports = { createAdminMessage };
